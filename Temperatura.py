8005e412da2b763c2f4f7a6c5197e0a39eed826c
StephenPeat <stephenpeat08@gmail.com>
2026-02-15 21:32:23 -0300

    Add files via upload

---
+++ Temperatura.py
@@ -0,0 +1,4 @@
+fahrenheit = int(input("Escreva a temperatura em Fahrenheit: "))
+celsius = (fahrenheit - 32) * (5/9)
+celsius = int(celsius)
+print(f"{fahrenheit} graus Fahrenheit equivalem a {celsius} graus Celsius.")
